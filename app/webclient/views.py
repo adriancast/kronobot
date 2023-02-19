bba8d3b86eb789a53207bbe731f12021817062ba
adriancast <adrian.castellanos>
2023-02-19 19:51:08 +0100

    Next events now return the next events :P

--- app/webclient/views.py
+++ app/webclient/views.py
@@ -4,15 +4,17 @@ from core.models import CompetitorModel
 from datetime import datetime
 
 
-def home(request):         
+def home(request):
     context = {
         "is_live": EventModel.objects.filter(date=datetime.today()).exists(),
-        "next_events": EventModel.objects.order_by('-date')[:5],
-        "rallyes": EventModel.objects.filter(category=EventCategory.RALLY).order_by('-date'),
-        "pujades": EventModel.objects.filter(category=EventCategory.HILL_CLIMB).order_by('-date'),
-        "karting": EventModel.objects.filter(category=EventCategory.KARTING).order_by('-date'),
-        "autocross": EventModel.objects.filter(category=EventCategory.AUTO_CROSS).order_by('-date'),
+        "next_events": EventModel.objects.filter(date__gte=datetime.today()).order_by("date")[:5],
+        "rallyes": EventModel.objects.filter(category=EventCategory.RALLY).order_by("-date"),
+        "pujades": EventModel.objects.filter(category=EventCategory.HILL_CLIMB).order_by("-date"),
+        "karting": EventModel.objects.filter(category=EventCategory.KARTING).order_by("-date"),
+        "autocross": EventModel.objects.filter(category=EventCategory.AUTO_CROSS).order_by("-date"),
         "competitors": CompetitorModel.objects.exclude(photo="")[:10],
-        "historic": sorted(EventModel.objects.values_list("date__year", flat=True).distinct(), reverse=True)
+        "historic": sorted(
+            EventModel.objects.values_list("date__year", flat=True).distinct(), reverse=True
+        ),
     }
-    return render(request, 'home.html', context)
+    return render(request, "home.html", context)
